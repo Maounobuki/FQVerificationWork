1206fcae2141851e37345f7e6dc25c6f508abdc4
Maou <waspofcrimea@yandex.ru>
2022-12-25 21:12:28 +0300

    solved objective

--- VerProj/Program.cs
+++ VerProj/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Укажите количество вводимых данных: ");
+int m = int.Parse(Console.ReadLine()!);
+string [] usdata = ArrInput(m);
+Console.WriteLine($"Массив:  [{String.Join(" , ",usdata)}]");
+int lenght = ThreeCharCount(usdata);
+string [] th = ArrInputThree(usdata, lenght);
+Console.WriteLine($"Массив:  [{String.Join(" , ",th)}]");
+
+
+
+
+
+//-------------methods-----------------------
+
+
+
+string [] ArrInput(int lenght)
+{
+string [] cell = new string [lenght];
+
+for (int i = 0; i<lenght; i++)
+{
+    Console.WriteLine("Введите значение: ");
+    cell[i] = Console.ReadLine()!;
+   }
+return cell;
+}
+ 
+ 
+ int ThreeCharCount (string [] arr )
+ {
+    int count = 0;
+    for (int i = 0; i<arr.Length; i++)
+    {
+    if(arr[i].Length<=3)
+    {
+        count++;
+    }
+    }
+    return count;
+ }
+
+ string [] ArrInputThree(string [] arr, int len )
+{
+string [] three = new string [len];
+int n = 0;
+for(int i = 0; i<arr.Length; i++)
+{
+    
+    if(arr[i].Length < 4)
+    {
+       three[n] = arr[i];
+         n++; 
+    }
+ }
+ return three;
+ }
